53511d5d508519c2b8ae844322ff0626ed29663e
neutralradiance <williamluke@me.com>
2020-10-30 22:38:41 -0500

    Set dependency

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["Storage"])
     ],
     dependencies: [
-         .package(path: "../Unwrap")
+         .package(url: "https://github.com/neutralradiance/swift-unwrap.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
