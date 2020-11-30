befcf96caf156c45e5e5831c7ffeea20c67aac38
neutralradiance <williamluke@me.com>
2020-11-30 17:07:59 -0600

    Delete CustomCodable

---
+++ Sources/Storage/Protocols/AutoCodable.swift
@@ -0,0 +1,32 @@
+//
+//  AutoCodable.swift
+//  
+//
+//  Created by neutralradiance on 10/21/20.
+//
+
+import Foundation
+import Combine
+
+/// An object that conforms to `AutoDecodable` & `AutoEncodable`.
+public typealias AutoCodable = AutoDecodable & AutoEncodable
+
+/// An object with a static, top level decoder.
+public protocol AutoDecodable: Codable {
+    associatedtype AutoDecoder: TopLevelDecoder
+
+    /// Decoder used for decoding a `AutoDecodable` object.
+    static var decoder: AutoDecoder { get }
+}
+
+/// An object with a static, top level encoder.
+public protocol AutoEncodable: Codable {
+    associatedtype AutoEncoder: TopLevelEncoder
+
+    /// Encoder used for encoding a `AutoEncodable` object.
+    static var encoder: AutoEncoder { get }
+}
+
+extension AutoEncodable {
+    public func encoded() throws -> AutoEncoder.Output { try Self.encoder.encode(self) }
+}
