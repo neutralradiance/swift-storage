d4d6e40c903d2d8a5f32f4c5c314123bdc55ed64
neutralradiance <williamluke@me.com>
2020-12-02 12:13:46 -0600

    Add Algorithms dependency

--- Package.swift
+++ Package.swift
@@ -16,14 +16,19 @@ let package = Package(
             targets: ["Storage"])
     ],
     dependencies: [
-         .package(url: "https://github.com/neutralradiance/swift-unwrap.git", from: "1.0.0")
+         .package(url: "https://github.com/neutralradiance/swift-unwrap.git", from: "1.0.0"),
+                  .package(url: "https://github.com/apple/swift-algorithms.git", from: "0.0.2")
+         
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Storage",
-            dependencies: ["Unwrap"]),
+            dependencies: [
+            "Unwrap",
+            .product(name: "Algorithms", package: "swift-algorithms")
+            ]),
         .testTarget(
             name: "StorageTests",
             dependencies: ["Storage", "Unwrap"])
